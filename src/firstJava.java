497c2fe0b41a1adfac9f6af8bd5f6e5a52786c9c
Daniel-Lohrey <daniel.lohrey@web.de>
2021-01-20 08:42:40 +0100

    Add Example

---
+++ src/firstJava.java
@@ -0,0 +1,11 @@
+public class firstJava {
+        public static void main(String[] args) {
+
+            int var1 = 4;
+            int var2 = 5;
+            boolean niceWeather = false;
+            System.out.println(var1 + var2);
+            System.out.println(niceWeather);
+        }
+
+}
